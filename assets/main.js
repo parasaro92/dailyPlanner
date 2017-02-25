69f8d7163c671107e45861673bb3348782d305e2
paras <rockypro92@gmail.com>
2017-02-25 14:53:49 +0530

    Use can edit and update task

--- assets/main.js
+++ assets/main.js
@@ -4,6 +4,10 @@ $(document).ready(function(){
     addTask(e);
   });
 
+  $("#edit-task-form").on('submit', function(e){
+    updateTask(e);
+  });
+
   displayTasks();
   
   //Function to display tasks
@@ -88,4 +92,97 @@ $(document).ready(function(){
       console.log("task add!");
     }
   }
+
+  // Update task
+
+  function updateTask(e) {
+
+    var id = $("#task_id").val();
+    var task = $("#task").val();
+    var task_date = $("#date").val();
+    var task_time = $("#time").val();
+    var task_priority = $("#priority").val();
+
+    var taskList = JSON.parse(localStorage.getItem('tasks'));
+
+    for(var i=0; i < taskList.length; i++){
+
+      if(taskList[i].id == id) {
+
+        taskList.splice(i,1);
+      }
+      localStorage.setItem('tasks', JSON.stringify(taskList));
+    }
+
+    if(task == ''){
+      alert('Task is required');
+      e.preventDefault();
+    } else if(task_date == ''){
+      alert('Date is required');
+      e.preventDefault();
+    } else if(task_time == ''){
+      alert('Time is required');
+      e.preventDefault();
+    } else if(task_priority == ''){
+      task_priority = 'normal';
+    } else {
+
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+
+      if (tasks == null){
+        tasks = [];
+     }
+
+      var taskList = JSON.parse(localStorage.getItem('tasks'));
+
+      var new_task = {
+        "id": id,
+        "task": task,
+        "task_priority": task_priority,
+        "task_date": task_date,
+        "task_time": task_time
+      }
+
+      tasks.push(new_task);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+
+      console.log("task add!");
+    }
+  }
 });
+
+  // GET single task
+  function getTask(){
+
+    var $_GET = getQueryParams(document.location.search);
+    id = $_GET['id'];
+
+    var taskList = JSON.parse(localStorage.getItem('tasks'));
+
+    for(var i=0; i < taskList.length; i++){
+
+      if(taskList[i].id == id){
+        $('#edit-task-form #task_id').val(taskList[i].id);
+        $('#edit-task-form #task').val(taskList[i].task);
+        $('#edit-task-form #priority').val(taskList[i].task_priority);
+        $('#edit-task-form #date').val(taskList[i].task_date);
+        $('#edit-task-form #time').val(taskList[i].task_time);
+      }
+    }
+  } 
+
+  // GET HTTP GET request
+  function getQueryParams(qs){
+
+    qs = qs.split("+").join(" ");
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while(tokens = re.exec(qs)){
+      params[decodeURIComponent(tokens[1])]
+        = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+  }
